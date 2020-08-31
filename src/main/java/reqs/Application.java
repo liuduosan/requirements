84c21be304edc713fe09fcf19b3689b24abe1f26
liuduosan@home <liuduosan@163.com>
2020-08-31 22:31:58 +0800

    基础框架，有点问题。

--- src/main/java/reqs/Application.java
+++ src/main/java/reqs/Application.java
@@ -2,7 +2,6 @@ package reqs;
 
 import java.util.Arrays;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication/*(scanBasePackages = {"reqs.dao"})*/
-@MapperScan("reqs.dao")
+/*@MapperScan("reqs.dao")*/
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/reqs/controller/ReqsController.java
+++ src/main/java/reqs/controller/ReqsController.java
@@ -1,6 +1,8 @@
 package reqs.controller;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.*;
 import reqs.dao.RequirementMapper;
 import reqs.vo.Requirement;
@@ -9,33 +11,34 @@ import reqs.vo.RequirementExample;
 import java.util.List;
 
 @RestController("/reqs")
+@MapperScan("reqs.dao")
 public class ReqsController {
 
     @Autowired
     private RequirementMapper requirementMapper;
-    /**
-     *
-     * @param requirement
-     * @return 1-ok,0-error
-     */
-    @PostMapping
-    public int addReqs(Requirement requirement){
-        return requirementMapper.insert(requirement);
-    }
-
-    @GetMapping("/{puter}")
-    @ResponseBody
-    public Requirement  getReqsByPuter(@PathVariable String puter){
-
-        RequirementExample example = new RequirementExample();
-        RequirementExample.Criteria criteria = example.createCriteria();
-        criteria.andPuterEqualTo(puter);
-        List<Requirement> list =  requirementMapper.selectByExample(example);
-        if(list.size()>0){
-            return list.get(0);
-        }else{
-            return  null;
-        }
-
-    }
+//    /**
+//     *
+//     * @param requirement
+//     * @return 1-ok,0-error
+//     */
+//    @PostMapping
+//    public int addReqs(Requirement requirement){
+//        return requirementMapper.insert(requirement);
+//    }
+//
+//    @GetMapping("/{puter}")
+//    @ResponseBody
+//    public Requirement  getReqsByPuter(@PathVariable String puter){
+//
+//        RequirementExample example = new RequirementExample();
+//        RequirementExample.Criteria criteria = example.createCriteria();
+//        criteria.andPuterEqualTo(puter);
+//        List<Requirement> list =  requirementMapper.selectByExample(example);
+//        if(list.size()>0){
+//            return list.get(0);
+//        }else{
+//            return  null;
+//        }
+//
+//    }
 }

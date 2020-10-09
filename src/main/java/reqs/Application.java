a8ec5975ec42ae5d1d1c7bb20f6c94ff72423d4d
kuaijiebang <lt@kuaijieqingjie.com>
2020-10-09 18:02:26 +0800

    解决跨域问题，还没解决完。

--- src/main/java/reqs/Application.java
+++ src/main/java/reqs/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication/*(scanBasePackages = {"reqs.dao"})*/
 /*@MapperScan("reqs.dao")*/
@@ -28,6 +31,25 @@ public class Application {
                 System.out.println(beanName);
             }
 
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("PUT", "DELETE","GET","POST")
+                        .allowedHeaders("*")
+                        .exposedHeaders("access-control-allow-headers",
+                                "access-control-allow-methods",
+                                "access-control-allow-origin",
+                                "access-control-max-age",
+                                "X-Frame-Options")
+                        .allowCredentials(false).maxAge(3600);
+            }
         };
     }
 
